2dea6141348f1a024c60652a82f11c88b4dfd652
AstonDV <astondv.kms@gmail.com>
2023-01-26 21:18:22 +1000

    End task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int GetSum(int A)
+{
+    int sum = 0;
+    if (A > 0)
+    {
+        for (int i = 1; i <= A; i++)
+        {
+            sum += i;
+        }
+    }
+    
+    return sum;
+}
+
+Console.Write("Введите А: ");
+int n = int.Parse(Console.ReadLine()!);
+int sum = GetSum(n);
+Console.WriteLine($"{sum}");
+
