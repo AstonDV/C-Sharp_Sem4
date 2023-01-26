e2699813138761e2833f3b265e9df8b0b5e9a371
AstonDV <astondv.kms@gmail.com>
2023-01-26 21:52:43 +1000

    End task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+void main()
+{
+    Console.Clear();
+    Console.WriteLine("Введите число: ");
+    int num = int.Parse(Console.ReadLine()!);
+    Console.WriteLine(Result(num));
+}
+
+
+int Result(int number)
+{
+    if (number < 0) number *= -1;
+    int count = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+main();
