bf0ba9e09424cd966a5aacfb2c793b1dfe1942e6
AstonDV <astondv.kms@gmail.com>
2023-01-26 22:08:36 +1000

    End task 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int GetMult(int A)
+{
+    int mult = 1;
+    if (A > 0)
+    {
+        for (int i = 1; i <= A; i++)
+        {
+            mult *= i;
+        }
+    }
+    
+    return mult;
+}
+
+Console.Write("Введите А: ");
+int n = int.Parse(Console.ReadLine()!);
+int mult = GetMult(n);
+Console.WriteLine($"{mult}");
